066939e697ad67d219fcf626dd3d5749d5b401e5
Daniel Morris <dan@Belobog.local>
2010-07-19 15:27:10 +0100

    moonshine setup stuffs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,3 +26,5 @@ config.action_view.cache_template_loading            = true
 
 # Enable threaded mode
 # config.threadsafe!
+# Use postfix for mail delivery 
+ActionMailer::Base.delivery_method = :sendmail 
