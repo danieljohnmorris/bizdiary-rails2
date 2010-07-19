7ad86a1fdbec03e87e46542670b6364c95d517b1
Daniel Morris <dan@Belobog.local>
2010-07-19 18:06:52 +0100

    add sphinx tasks to cap deploy conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,19 @@
 server '173.203.95.138', :app, :web, :db, :primary => true
+
+### SEARCH - THINKING SPHINX 
+
+require 'vendor/plugins/thinking-sphinx/recipes/thinking_sphinx'
+
+task :before_update_code, :roles => [:app] do
+  thinking_sphinx.stop
+end
+
+task :after_update_code, :roles => [:app] do
+  symlink_sphinx_indexes
+  thinking_sphinx.configure
+  thinking_sphinx.start
+end
+
+task :symlink_sphinx_indexes, :roles => [:app] do
+  run "ln -nfs #{shared_path}/db/sphinx #{release_path}/db/sphinx"
+end
