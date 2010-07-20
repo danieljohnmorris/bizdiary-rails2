22cf04c1b52cba26c136fc5dc1c3181d8445db12
Daniel Morris <dan@Belobog.local>
2010-07-20 15:01:46 +0100

    change release_path refs to current_path - here's hoping this doesn't cause more issues later!

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,44 +5,44 @@ server '173.203.95.138', :app, :web, :db, :primary => true
 # Thinking Sphinx
 namespace :thinking_sphinx do
   task :configure, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
   end
   task :index, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
   end
   task :start, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
   end
   task :stop, :roles => [:app] do
     run "cd #{current_path}; rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
   end
   task :restart, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
   end
   task :rebuild, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
   end
 end
 
 # Thinking Sphinx typing shortcuts
 namespace :ts do
   task :conf, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:configure RAILS_ENV=#{rails_env}"
   end
   task :in, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:index RAILS_ENV=#{rails_env}"
   end
   task :start, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:start RAILS_ENV=#{rails_env}"
   end
   task :stop, :roles => [:app] do
     run "cd #{current_path}; rake thinking_sphinx:stop RAILS_ENV=#{rails_env}"
   end
   task :restart, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:restart RAILS_ENV=#{rails_env}"
   end
   task :rebuild, :roles => [:app] do
-    run "cd #{release_path}; rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; rake thinking_sphinx:rebuild RAILS_ENV=#{rails_env}"
   end
 end
 
