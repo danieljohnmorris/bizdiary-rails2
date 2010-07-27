5d0638f723d1b8e55814ae1e892445988a0af863
Daniel Morris <dan@belobog.lan>
2010-07-27 23:54:15 +0100

    put 2 lines onto 1

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,7 @@
 class Person < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :rpx_connectable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :rpx_connectable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
