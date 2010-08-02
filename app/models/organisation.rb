6b78ca47d7a6f74d3af8edfe0206361df5b5cd96
Daniel Morris <dan@belobog.lan>
2010-08-02 01:58:24 +0100

    devise_invitable

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,5 +1,5 @@
 class Organisation < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :rpx_connectable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :invitable, :rpx_connectable
   has_many :events
   acts_as_taggable_on :saves
 
